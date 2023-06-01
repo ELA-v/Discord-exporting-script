fcd387b58cbba6393f98af88bd29a26a31d3f115
ELAvestruz <84411624+ELA-v@users.noreply.github.com>
2023-06-01 16:25:12 -0700

    Add files via upload

---
+++ Weekly-Export2-InteractiveTEMPLATE-Github.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+## This script will make a whole server export of the past 7 days. ##TOKEN AND OUTPUT DIRECTORY NEED TO BE FILLED OUT MANUALLY FIRST##
+## 
+## EXPORT_DATE export made possible by: https://stackoverflow.com/questions/65154428/how-to-echo-text-containing-with-double-quotes.
+##
+## Converting to epoch time taken from: https://stackoverflow.com/questions/10990949/convert-date-time-string-to-epoch-in-bash And from: https://www.oreilly.com/library/view/bash-cookbook/0596526784/ch11s04.html (Second link is the best one).
+##
+## Examples of dates, from the coreutils manual: https://www.gnu.org/software/coreutils/manual/html_node/Examples-of-date.html A reduced version of the manual is available as a manpage for date in linux.
+##
+## This is a way to input single quotes without breaking a variable call:
+##                  \'${Input}\'
+##
+##
+## DATE="`date +%B-%d-%Y`"
+##
+EXPORT_DATE=`(date --date="last week UTC" '+%Y-%m-%d-00:00')` ## Will output the date of 7 days ago of your current time at 00:00, adjusted to the timezone (UTC in this case, change as you need). 
+LASTWEEK=`(date --date="last week" +%B-%d-%Y)`
+TODAY="`(date +%B-%d-%Y)`"
+## Different date format: (date --date="last week" +'%Y-%m-%d-%H·%M%p')`
+
+## Paste your data inside the single quotes.
+SERVER_ID='' ## No need to fill this part
+TOKEN='' ## Important: keep token between ''
+OUTPUT_DIRECTORY="" ## Make sure this path doesn't have a slash at the end. // No need to fill this part
+CHAT_EXPORTER_DIRECTORY='' ## The directory of the chat exporting tool.  Keep a slash at the end for this directory. Keep the path between ''
+
+cd ${CHAT_EXPORTER_DIRECTORY}
+
+echo Enter server ID
+read SERVER_ID
+echo Enter output directory
+read OUTPUT_DIRECTORY
+
+dotnet DiscordChatExporter.Cli.dll exportguild -g ${SERVER_ID} --after ${EXPORT_DATE} --parallel 2 -p 100mb -t ${TOKEN} -o "${OUTPUT_DIRECTORY}/%G [${LASTWEEK}] - [${TODAY}]/[%P] %T/[%p] %C.html"
+
+## Having other types of quotes for the output path breaks the tool, using the example of line 11 messes up the path, having spaces in the export date breaks the tool too.
+## No slashes, colons, or asterisks are allowed on paths. "|" is also not allowed
+##
+## FOLDER STRUCTURE: The script creates the following directory structure.
+##
+##  /'Server name' [Last week date]-[Today's date]/
+##           |---> [Category position] Category name/
+##                |---> [Channel position] Channel name.html
+##                |---> [Channel position] Channel name.html
+##
